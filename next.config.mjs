fe60d2df744829b27fd579bacb6f770821494643
yunsusu <rladbstn1212@gmail.com>
2024-11-05 22:41:19 +0900

    기초세팅2

---
+++ next.config.mjs
@@ -0,0 +1,35 @@
+import process from "node:process";
+
+import withPWA from "next-pwa";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  sassOptions: {
+    additionalData: '@import "@/styles/main.scss";',
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "sangchu-bucket.s3.ap-northeast-2.amazonaws.com",
+        port: "",
+        pathname: "/binder/img/**",
+      },
+      {
+        protocol: "https",
+        hostname: "binder-bucket.s3.ap-northeast-2.amazonaws.com",
+        port: "",
+        pathname: "/img/**",
+      },
+    ],
+  },
+  // 다른 Next.js 설정들
+};
+
+export default withPWA({
+  dest: "public", // PWA를 위한 service worker 파일이 생성될 경로
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development", // 개발 모드에서는 PWA 비활성화
+})(nextConfig);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import classNames from "classnames/bind";
+
+import styles from "./main.module.scss";
+
+const cn = classNames.bind(styles);
+
 export default function Home() {
-  return <div></div>;
+  return <div className={cn("page")}>test</div>;
 }
