24ed2c48a0011302c0f6e4ef11439963cd6d9af7
JunCastle <juncastle97@gmail.com>
2024-11-18 19:08:16 +0900

    feat : 디자인 시스템 설정 확인

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,5 +5,10 @@ import styles from "./main.module.scss";
 const cn = classNames.bind(styles);
 
 export default function Home() {
-  return <div className={cn("page")}>test</div>;
+  return (
+    <>
+      <div className={cn("page")}>test</div>
+      <div className={cn("text")}>글입니다.</div>
+    </>
+  );
 }
