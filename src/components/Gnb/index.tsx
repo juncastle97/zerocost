db71ab07c1b690589cbcf4062b3e71a37bc53a0f
yunsusu <rladbstn1212@gmail.com>
2024-11-19 21:55:09 +0900

    feat : gnb

--- src/components/Gnb/index.tsx
+++ src/components/Gnb/index.tsx
@@ -1,5 +1,22 @@
-"use client";
+import classNames from "classnames/bind";
+import Image from "next/image";
+import { useState } from "react";
+
+import pig from "@/../public/icons/ic-logo.svg";
+import setting from "@/../public/icons/ic-setting.svg";
+
+import style from "./gnb.module.scss";
+
+const cn = classNames.bind(style);
 
 export default function Gnb() {
-  return <></>;
+  const [gnbMore, setGnbMore] = useState();
+  return (
+    <div className={cn("gnbWrap")}>
+      <Image src={pig} alt={"로고"} width={40} height={40} />
+      <div>{gnbMore}</div>
+
+      <Image src={setting} alt={"메뉴"} width={30} height={30} />
+    </div>
+  );
 }
