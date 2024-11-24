7266892aea104e07d3f2e9aff89827e564991c77
yunsusu <rladbstn1212@gmail.com>
2024-11-24 18:06:52 +0900

    feat : 로그인 메인 화면 제작

---
+++ src/app/login/page.tsx
@@ -0,0 +1,54 @@
+"use client";
+import logo from "@/../public/icons/ic-logo.svg";
+import google from "@/../public/icons/icon_google.svg";
+import kakao from "@/../public/icons/icon_kakaotalk.svg";
+import { loginState } from "@/lib/atoms/login";
+import classNames from "classnames/bind";
+import { useAtom } from "jotai";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import styles from "./login.module.scss";
+
+const cn = classNames.bind(styles);
+
+export default function Login() {
+  const [login, setLogin] = useAtom(loginState);
+  const router = useRouter();
+
+  return (
+    <div className={cn("loginWrap")}>
+      <div className={cn("title")}>
+        <Image src={logo} alt="logo" width={130} height={130} />
+        <p>소비의 가치를 지키다</p>
+        <h1>ZERO COST</h1>
+      </div>
+
+      <div className={cn("loginBox")}>
+        <div className={cn("loginBtn")}>
+          <Image src={kakao} alt="kakao login" width={24} height={24} />
+          <p>카카오 시작하기</p>
+        </div>
+        <div className={cn("loginBtn")}>
+          <Image src={google} alt="google login" width={24} height={24} />
+          <p>구글로 시작하기</p>
+        </div>
+        <div
+          className={cn("guest")}
+          onClick={() => {
+            router.push("/");
+            setLogin(true);
+          }}
+        >
+          게스트로 시작하기
+        </div>
+      </div>
+
+      <div className={cn("agreeWrap")}>
+        계속 진행함에 따라 Zerocost의 <span>이용약관</span>과{" "}
+        <span>개인정보 보호정책</span>에
+        <br />
+        동의하는 것으로 간주됩니다.
+      </div>
+    </div>
+  );
+}
