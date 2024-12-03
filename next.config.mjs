db9440909b79aac7c3a4af0a19db61646e40f58f
yunsusu <rladbstn1212@gmail.com>
2024-12-03 20:49:01 +0900

    fix : 빌드 에서 수정

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,9 @@ import withPWA from "next-pwa";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    appDir: true, // app 디렉토리 사용 여부 확인
+  },
   reactStrictMode: true,
   sassOptions: {
     additionalData: '@import "@/styles/main.scss";',
--- src/components/commons/Gnb/index.tsx
+++ src/components/commons/Gnb/index.tsx
@@ -1,36 +1,31 @@
+"use client";
 import classNames from "classnames/bind";
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import pig from "@/../public/icons/ic-logo.svg";
 import user from "@/../public/icons/icon_user.svg";
-
-// import { useRouter } from "next/navigation";
 import styles from "./gnb.module.scss";
 
 const cn = classNames.bind(styles);
 
 export default function Gnb() {
-  // const loginState = localStorage.getItem("loginState");
-  const [gnbMore] = useState();
+  const [isMypage, setIsMypage] = useState(false);
 
-  // const router = useRouter();
-
-  // useEffect(() => {
-  //   if (loginState === "false" || loginState === null) {
-  //     router.push("/login");
-  //   }
-  // }, [loginState]);
+  useEffect(() => {
+    // 클라이언트 환경에서만 실행
+    if (typeof window !== "undefined") {
+      setIsMypage(window.location.pathname === "/mypage");
+    }
+  }, []);
 
   return (
     <div className={cn("gnbWrap")}>
       <Link href={"/"}>
         <Image src={pig} alt={"로고"} width={40} height={40} />
       </Link>
-      <div>{gnbMore}</div>
-
-      {window.location.pathname !== "/mypage" && (
+      {!isMypage && (
         <Link href={"/mypage"}>
           <Image src={user} alt={"메뉴"} width={30} height={30} />
         </Link>
