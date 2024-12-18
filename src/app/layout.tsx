36fe1ec56870fb162f20825f02039d1f432ef96a
JunCastle <juncastle97@gmail.com>
2024-12-18 21:11:54 +0900

    fix : 구글 애널리틱스 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,22 +23,21 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Google Tag Manager */}
+        {/* Google Analytics */}
         <Script
-          id="gtm-script"
-          strategy="beforeInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-KHC7QML4');
-            `,
-          }}
+          src="https://www.googletagmanager.com/gtag/js?id=G-EVEY14179F"
+          strategy="afterInteractive"
         />
-        {/* End Google Tag Manager */}
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-EVEY14179F');
+          `}
+        </Script>
+        {/* End Google Analytics */}
         <title>제로코스트</title>
         <link rel="icon" href={`/images/favicon.ico`} />
         <Script
@@ -81,16 +80,6 @@ export default function RootLayout({
         <link rel="apple-touch-icon" href="/favicon.ico" />
       </head>
       <body>
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-KHC7QML4"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
-        {/* End Google Tag Manager (noscript) */}
         <QueryClientProvider client={queryClient}>
           <div
             style={{
